66df0a11976b531e8f35cb769a896f2ad453880d
Christopher Perrin <perrin@uni-trier.de>
2016-01-07 02:03:30 +0100

    Added upload stage with tag version

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(name='PAMMysqlTools',
-      version='0.0.1',
+      version='{tag}',
       description='A set of tools to manage users from pam_mysql',
       author='Christopher Perrin',
       author_email='perrin@uni-trier.de',
       url='https://gitlab.uni-trier.de/sad-team/PAMMySQL-Tools',
       packages=find_packages(),
       test_suite='tests',
-      install_requires=['pymysql', 'click'],
+      install_requires=['pymysql', 'click', 'setuptools-git-version'],
       entry_points={
           'console_scripts': [
               'myuseradd=pammymanager.scripts:useradd',
