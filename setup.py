1420d9ccbc4931c83d6edcbb2c1eb06dbdb5cbe0
Christopher Perrin <perrin@uni-trier.de>
2016-01-07 03:15:27 +0100

    Fixed classifiers again in setup.py

--- setup.py
+++ setup.py
@@ -26,6 +26,6 @@ setup(name='PAMMySQLTools',
           'pammymanager': ['*.mo']
       },
       classifiers=[
-          "LICENSE :: OSI Approved :: MIT License",
+          "License :: OSI Approved :: MIT License",
           "Environment :: Console"
       ])
